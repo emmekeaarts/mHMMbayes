914891f48d02e0d26826cb10ed97d7d562981be4
Emmeke Aarts <e.aarts@uu.nl>
2019-10-06 19:15:28 +0200

    adjusted example tutorial datasets such that package does not become too large

--- data-raw/creating tutorial output data.R
+++ data-raw/creating tutorial output data.R
@@ -22,6 +22,13 @@ out1 <- mHMM(s_data = nonverbal,
                  start_val = c(list(start_TM), start_EM),
                  mcmc = list(J = 1000, burn_in = 200))
 
+# for this dataset, we use:
+# PD_subj
+# gamma_prob_bar
+# emiss_prob_bar
+
+# check, still delete not used slots
+
 save(out1, file = "nonv_2st_1000it.rda")
 
 
@@ -51,6 +58,16 @@ out1b <- mHMM(s_data = nonverbal,
                   gen = list(m = m, n_dep = n_dep,q_emiss = q_emiss),
                   start_val = c(list(start_TM), start_EM_b),
                   mcmc = list(J = 1000, burn_in = 200))
+
+out1b$PD_subj <- NULL
+out1b$emiss_cov_bar <-NULL
+out1b$emiss_int_bar <-NULL
+out1b$emiss_int_subj <-NULL
+out1b$emiss_naccept <-NULL
+out1b$gamma_cov_bar <-NULL
+out1b$gamma_int_bar <-NULL
+out1b$gamma_int_subj <-NULL
+out1b$gamma_naccept <-NULL
 
 save(out1b, file = "nonv_2stb_1000it.rda")
 
@@ -85,6 +102,8 @@ out2 <- mHMM(s_data = nonverbal,
                  gen = list(m = m, n_dep = n_dep, q_emiss = q_emiss),
                  start_val = c(list(start_TM), start_EM),
                  mcmc = list(J = 1000, burn_in = 200))
+
+# for this data, we only use the AIC, no real data, do not include in package!
 
 save(out2, file = "nonv_3st_1000it.rda")
 
@@ -122,6 +141,20 @@ out3 <- mHMM(s_data = nonverbal,
                  gen = list(m = m, n_dep = n_dep, q_emiss = q_emiss),
                  start_val = c(list(start_TM), start_EM),
                  mcmc = list(J = 1000, burn_in = 200))
+
+# for this dataset, we only use:
+# gamma_prob_bar
+# emiss_prob_bar
+
+out3$PD_subj <-NULL
+out3$emiss_cov_bar <-NULL
+out3$emiss_int_bar <-NULL
+out3$emiss_int_subj <-NULL
+out3$emiss_naccept <-NULL
+out3$gamma_cov_bar <-NULL
+out3$gamma_int_bar <-NULL
+out3$gamma_int_subj <-NULL
+out3$gamma_naccept <-NULL
 
 save(out3, file = "nonv_4st_1000it.rda")
 
