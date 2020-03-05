c5ed3fed36af28c9557c2f90231a00ad29412ffe
Emmeke Aarts <e.aarts@uu.nl>
2020-03-05 11:13:24 +0100

    Restored old version of mnlHess_int, new version did not work for 2 states / 2 categorical observed categories

--- R/mnl_hess.R
+++ R/mnl_hess.R
@@ -1,6 +1,27 @@
 #' @keywords internal
 # Obtain mnl -Expected[Hessian]  for intercept only model, bassed on P.Rossi 2004
 mnlHess_int <- function(int, Obs, n_cat){
+  k 			<- n_cat - 1
+  n_Obs 	<- length(Obs)
+  Xint 		<- matrix(c(0, int), byrow = T, ncol = n_cat, nrow = n_Obs)
+  Xint		<- exp(Xint)
+  iota		<- c(rep(1,n_cat))
+  denom		<- Xint %*% iota
+  Prob		<- Xint / as.vector(denom)
+  Hess    <- matrix(double(k * k), ncol = k)
+  for (hi in 1:n_Obs) {
+    p     <- as.vector(Prob[hi, ])
+    A     <- diag(p) - outer(p, p)
+    Xt    <- diag(n_cat)[, -1]
+    Hess  <- Hess + crossprod(Xt, A) %*% Xt
+  }
+  return(Hess)
+}
+
+
+#' @keywords internal
+# faster version but dus not work for n_cat = 2, still repair
+mnlHess_int2 <- function(int, Obs, n_cat){
   n_Obs 	<- length(Obs)
   betas   <- c(0, int)
   prob    <- exp(betas) / sum(exp(betas))
