bfb3b412478044f53c51a35f5888c61e4226fb7d
Emmeke Aarts <emmeke@Emmekes-MacBook-Pro.local>
2021-12-10 09:55:19 +0100

    function to obran probabiliets of gamma as function of time varying covariates

---
+++ R/timevar_gamma.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+# obtain probabilities of gamma as a function of time varying covariates
+# returns gamma as a matrix with elements of gamma in the columns and time in rows.
+# Hence, m * m columns, and n_t rows.
+
+#timevar_gamma <- function(m, xx_t, ){
+
+
+#}
