d2bb62b9ffb470d47c104593fa25b6f8bfd09f08
Emmeke Aarts <emmeke@Emmekes-MacBook-Pro.local>
2022-01-14 10:27:08 +0100

    RW for beta now returns probability as well besides intercept, can be seen as the probabilities when predictor x equals 0

--- R/mnl_RW_once.R
+++ R/mnl_RW_once.R
@@ -44,11 +44,13 @@ mnl_RW_once_int_fixed_bet <- function(int1, Obs, xx_t, n_cat, mu_int_bar1, bet, 
   # obtain likelihood and transition prob with the parameters sampled in the previous iteration and current sampled state sequence
   oldloglike	 		<- llmnl_bet(bet = c(int1, bet), Obs = Obs, xx_t = xx_t, n_cat = n_cat)
   oldpostlike	 		<- oldloglike + dmvnorm(int1, mu_int_bar1, V_int1, log = TRUE)
+  probold				  <- int_to_prob(int1)
 
   # obtain new parameters for gamma from proposal distribution plus new likelihood
   int_new		 		  <- int1 + rmvnorm(1, rep(0, (n_cat - 1)), scalar^2 * candcov1, method = "svd")
   newloglike	 		<- llmnl_bet(bet = c(int_new, bet), Obs = Obs, xx_t = xx_t, n_cat = n_cat)
   newpostlike	 		<- newloglike + dmvnorm(int_new, mu_int_bar1, V_int1, log = TRUE)
+  probnew				  <- int_to_prob(int_new)
 
   # determine to use the updated or current (previous iteration) gamma values of the parameters
   acc 				   <- min(log(1), (newpostlike - oldpostlike))
@@ -60,25 +62,27 @@ mnl_RW_once_int_fixed_bet <- function(int1, Obs, xx_t, n_cat, mu_int_bar1, bet, 
   if (unif <= acc) {
     draw_int		<- int_new
     accept			<- 1
+    prob			  <- probnew
   } else {
     draw_int		<- int1
     accept			<- 0
+    prob			  <- probold
   }
-  return(list(draw_int = draw_int, accept = accept))
+  return(list(draw_int = draw_int, accept = accept, prob = prob))
 }
 
 
 #' @keywords internal
 # one run of the random walk metropolis sampler for an multinomial distribution
-# with a fixed beta for (one) time-varying covariate, in addtion to random intercept.
+# with a fixed beta for (one) time-varying covariate, in addition to random intercept.
 # The random intercepts are not estimated here, but incorporated when obtaining the
 # log likelihood estimates for the fixed beta
 
 mnl_RW_once_fixed_bet_pooled <- function(bet1, Obs_p, xx_t_p, n_cat, mu_bet_bar1, int_p, V_bet1, scalar, candcov2, n_subj) {
   # obtain likelihood and transition prob with the parameters sampled in the previous iteration and current sampled state sequence
-  bet_p            <- cbind(int_p, matrix(bet1, byrow = TRUE, ncol = n_cat - 1, nrow = n_subj))
+  bet_p           <- cbind(int_p, matrix(bet1, byrow = TRUE, ncol = n_cat - 1, nrow = n_subj))
   oldloglike	 		<- llmnl_bet_pooled(bet_p = bet_p, Obs_p = Obs_p, xx_t_p = xx_t_p, n_cat = n_cat, n_subj = n_subj)
-  oldpostlike	 		<- oldloglike + dmvnorm(bet, mu_bet_bar1, V_bet1, log = TRUE)
+  oldpostlike	 		<- oldloglike + dmvnorm(unlist(bet1), mu_bet_bar1, V_bet1, log = TRUE)
 
   # obtain new parameters for gamma from proposal distribution plus new likelihood
   bet_new		 		  <- bet1 + rmvnorm(1, rep(0, (n_cat - 1)), scalar^2 * candcov2, method = "svd")
