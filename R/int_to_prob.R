0c4a688353990d808aae6486ff9dc0fb368dc6a7
emmekeaarts <e.aarts@uu.nl>
2022-08-15 17:09:38 +0200

    making int_to_prob and prob_to_int external including help file, making seperate functions for setting hyper-prior values for gamma and emiss, making seprate functions for setting RW metropolis sampler on gamma and emiss

--- R/int_to_prob.R
+++ R/int_to_prob.R
@@ -1,32 +1,117 @@
+#' Transforming a set of Multinomial logit regression intercepts to probabilities
+#'
+#' \code{int_to_prob} transforms a set of Multinomial logit regression
+#' intercepts to the corresponding probabilities for each state or category.
+#' Note that the first state or category is assumed to be the reference category, hence
+#' no intercept is to specified for the first state or category.
+#'
+#' Designed to ease the specification of informative hyper-prior values for the
+#' mean intercepts of the transition probability matrix gamma and categorical
+#' emission distribution(s) of the multilevel hidden Markov model through the
+#' functions \code{\link{prior_gamma}} and \code{\link{prior_emiss_cat}}. No
+#' check is performed on correct specifications of the dimensions.
+#'
+#' @param int_matrix A matrix with (number of states OR categories - 1) columns
+#'   and number of rows to be determined by the user. For obtaining the set of
+#'   probabilities of the complete transition probability matrix gamma or
+#'   categorical emission distribution matrix, the number of rows equals the
+#'   number of states \code{m}. The first state / category is assumed to be the
+#'   reference category, no intercept is to be specified for this first
+#'   category.
+#'
+#' @return \code{int_to_prob} returns a matrix containing probabilities with
+#'   each row summing to one, with the number of columns equal to the number of
+#'   states / categories and the number of rows equal to the number rows
+#'   specified in the input matrix.
+#'
+#' @seealso \code{\link{prob_to_int}} for transforming a set of probabilities to
+#'   a set of Multinomial logit regression intercepts, \code{\link{prior_gamma}}
+#'   and \code{\link{prior_emiss_cat}} for specifying informative hyper-priors
+#'   for the the multilevel hidden Markov model and \code{\link{mHMM}} to fit a
+#'   multilevel hidden Markov model.
+#'
+#' @examples
+#'
+#' # example for transition probability matrix gamma with 3 states
+#' m <- 3
+#' gamma_int <- matrix(c(-1, -1,
+#'                        3,  0,
+#'                        0,  2), ncol = m-1, nrow = m, byrow = TRUE)
+#' gamma_prob <- int_to_prob(gamma_int)
+#' gamma_prob
+#'
+#' @export
 
-#' @keywords internal
-# computes probabilities from intercepts
-int_to_prob <- function(int1) {
-  if(is.matrix(int1)){
-    prob1 <- matrix(nrow = nrow(int1), ncol = ncol(int1) + 1)
-    for(r in 1:nrow(int1)){
-      exp_int1 	<- matrix(exp(c(0, int1[r,])), nrow  = 1)
-      prob1[r,] <- exp_int1 / as.vector(exp_int1 %*% c(rep(1, (dim(exp_int1)[2]))))
-    }
-  } else {
-    exp_int1 	<- matrix(exp(c(0, int1)), nrow  = 1)
-    prob1 		<- exp_int1 / as.vector(exp_int1 %*% c(rep(1, (dim(exp_int1)[2]))))
+int_to_prob <- function(int_matrix) {
+  if(!is.matrix(int_matrix)){
+    stop("int_matrix should be a matrix")
   }
-  return(round(prob1,4))
+  prob_matrix <- matrix(nrow = nrow(int_matrix), ncol = ncol(int_matrix) + 1)
+  for(r in 1:nrow(int_matrix)){
+    exp_int_matrix 	<- matrix(exp(c(0, int_matrix[r,])), nrow  = 1)
+    prob_matrix[r,] <- exp_int_matrix / as.vector(exp_int_matrix %*% c(rep(1, (dim(exp_int_matrix)[2]))))
+    }
+  return(round(prob_matrix,4))
 }
 
-# computes intercepts from probabilities, per row of input matrix
-# first catagory is reference catagory
-prob_to_int <- function(prob1){
-  prob1 <- prob1 + 0.00001
-  b0 <- matrix(NA, nrow(prob1), ncol(prob1)-1)
-  sum_exp <- numeric(nrow(prob1))
-  for(r in 1:nrow(prob1)){
-    sum_exp[r] <- (1/prob1[r,1]) - 1
-    for(cr in 2:ncol(prob1)){
-      #for every b0 except the first collumn (e.g. b012 <- log(y12/y11-y12))
-      b0[r,(cr-1)] <- log(prob1[r,cr]*(1+sum_exp[r]))
+#' Transforming a set of probabilities to Multinomial logit regression intercepts
+#'
+#' \code{prob_to_int} transforms a set of state or observation probabilities to the
+#' corresponding Multinomial logit regression intercepts. Note that the first
+#' category is assumed to be the reference category, hence no intercept is
+#' returned for the first state or category.
+#'
+#' Designed to ease the specification of informative hyper-prior values for the
+#' mean intercepts of the transition probability matrix gamma and categorical
+#' emission distribution(s) of the multilevel hidden Markov model through the
+#' functions \code{\link{prior_gamma}} and \code{\link{prior_emiss_cat}}. No
+#' check is performed on correct specifications of the dimensions.
+#'
+#' @param prob_matrix A matrix with number of states OR categories columns and
+#'   number of rows to be determined by the user, with rows summing to one. For
+#'   obtaining the set of Multinomial logit regression intercepts of the
+#'   complete transition probability matrix gamma or categorical emission
+#'   distribution matrix, the number of rows equals the number of states
+#'   \code{m}.
+#'
+#' @return \code{prob_to_int} returns a matrix containing Multinomial logit
+#'   regression intercepts, with the number of columns equal to (number of
+#'   states or categories - 1) and the number of rows equal to the number rows
+#'   specified in the input matrix. The first state / category is assumed to be
+#'   the reference category, hence no intercept is returned for this first
+#'   category.
+#'
+#' @seealso \code{\link{int_to_prob}} for transforming a set of Multinomial
+#'   logit regression intercepts to a probabilities, \code{\link{prior_gamma}}
+#'   and \code{\link{prior_emiss_cat}} for specifying informative hyper-priors
+#'   for the the multilevel hidden Markov model and \code{\link{mHMM}} to fit a
+#'   multilevel hidden Markov model.
+#'
+#' @examples
+#'
+#' # example for transition probability matrix gamma with 3 states
+#' m <- 3
+#' gamma_prob <- matrix(c(0.6, 0.2, 0.2,
+#'                        0.1, 0.8, 0.1,
+#'                        0.1, 0.1, 0.8), ncol = m, nrow = m, byrow = TRUE)
+#' gamma_int <- prob_to_int(gamma_prob)
+#' gamma_int
+#'
+#' @export
+
+prob_to_int <- function(prob_matrix){
+  if(!is.matrix(prob_matrix)){
+    stop("prob_matrix should be a matrix")
+  }
+  prob_matrix <- prob_matrix + 0.00001
+  int_matrix <- matrix(NA, nrow(prob_matrix), ncol(prob_matrix)-1)
+  sum_exp <- numeric(nrow(prob_matrix))
+  for(r in 1:nrow(prob_matrix)){
+    sum_exp[r] <- (1/prob_matrix[r,1]) - 1
+    for(cr in 2:ncol(prob_matrix)){
+      #for every int_matrix except the first collumn (e.g. int_matrix12 <- log(y12/y11-y12))
+      int_matrix[r,(cr-1)] <- log(prob_matrix[r,cr]*(1+sum_exp[r]))
     }
   }
-  return(round(b0,4))
+  return(round(int_matrix,4))
 }
--- R/prior_gamma.R
+++ R/prior_gamma.R
@@ -1,31 +1,47 @@
 #' Specifying informative hyper-prior on the transition probability matrix gamma of the multilevel hidden Markov model
 #'
-#' \code(prior_gamma) provides a framework to manually specify an informative
+#' \code{prior_gamma} provides a framework to manually specify an informative
 #' hyper-prior on the transition probability matrix gamma, and creates an object
-#' of class mHMM_prior_gamma used by the function \code{mHMM}. Estimation of the
-#' mHMM proceeds within a Bayesian context, hence a hyper-prior distribution has
-#' to be defined for the group level parameters. Default, non-informative priors
-#' are used unless specified otherwise by the user. Note that the hyper-prior
-#' distribution on the transition probabilities are on the intercepts (and, if
-#' subject level covariates are used, regression coefficients) of the
-#' Multinomial regression model used to accommodate the multilevel framework of
-#' the data, instead of on the probabilities directly. See the function
-#' \code{\link{prob_to_int}} and \code{\link{int_to_prob}} for translating
-#' probabilities to a set of Multinomial regression intercepts and vice versa.
-#' Each row of the transition probability matrix has its own set of Multinomial
-#' regression intercepts, which are assumed to follow a multivariate normal
-#' distribution. Hence, the hyper-prior distributions for the intercepts
+#' of class \code{mHMM_prior_gamma} used by the function \code{mHMM}. Note that the
+#' hyper-prior distribution on the transition probabilities are on the
+#' intercepts (and, if subject level covariates are used, regression
+#' coefficients) of the Multinomial logit model used to accommodate the
+#' multilevel framework of the data, instead of on the probabilities directly.
+#' The set of hyper-prior distributions consists of a multivariate Normal
+#' hyper-prior distribution on the vector of means (i.e., intercepts and
+#' regression coefficients), and an Inverse Wishart hyper-prior distribution on
+#' the covariance matrix.
+#'
+#' Estimation of the mHMM proceeds within a Bayesian context, hence a
+#' hyper-prior distribution has to be defined for the group level parameters.
+#' Default, non-informative priors are used unless specified otherwise by the
+#' user. Each row of the transition probability matrix has its own set of
+#' Multinomial logit intercepts, which are assumed to follow a multivariate
+#' normal distribution. Hence, the hyper-prior distributions for the intercepts
 #' consists of a multivariate Normal hyper-prior distribution on the vector of
 #' means, and an Inverse Wishart hyper-prior distribution on the covariance
-#' matrix.
+#' matrix. Note that only the number of states \code{m} and values of the
+#' hypothesized hyper-prior mean values of the Multinomial logit intercepts have
+#' to be specified by the user, default values are available for all other
+#' hyper-prior distribution parameters.
+#'
+#' Given that the hyper-priors are specified on the intercepts of the Multinomial
+#' logit model intercepts instead of on the probabilities of the transition
+#' probability matrix gamma directly, specifying a hyper-prior can seem rather
+#' daunting. However, see the function \code{\link{prob_to_int}} and
+#' \code{\link{int_to_prob}} for translating probabilities to a set of
+#' Multinomial logit intercepts and vice versa.
 #'
 #' Note that \code{gamma_K0}, \code{gamma_nu} and \code{gamma_V} are assumed
-#' equal over the states. When the hyper-prior on the transition probability
-#' matrix gamma is not manually specified, the default values are as follows.
-#' All elements of the matrices contained in \code{gamma_mu0} are set to 0,
-#' \code{gamma_K0} set to 1, \code{gamma_nu} set to 3 + m - 1, and the diagonal
-#' of \code{gamma_V} (i.e., the variance) set to 3 + m - 1 and the off-diagonal
-#' elements (i.e., the covariance) set to 0.
+#' equal over the states. When the hyper-prior values for \code{gamma_K0},
+#' \code{gamma_nu} and \code{gamma_V} are not manually specified, the default
+#' values are as follows. \code{gamma_K0} set to 1, \code{gamma_nu} set to 3 + m
+#' - 1, and the diagonal of \code{gamma_V} (i.e., the variance) set to 3 + m - 1
+#' and the off-diagonal elements (i.e., the covariance) set to 0. In addition,
+#' when no manual values for the hyper-prior on gamma are specified at all (that
+#' is, the function \code{prior_gamma} is not used), all elements of the
+#' matrices contained in \code{gamma_mu0} are set to 0 in the function
+#' \code{mHMM}.
 #'
 #' Note that in case covariates are specified, the hyper-prior parameter values
 #' of the inverse Wishart distribution on the covariance matrix remain
@@ -33,32 +49,68 @@
 #' are fixed over subjects.
 #'
 #' @param m Numeric vector with length 1 denoting the number of hidden states.
-#' @param n_xx Optional numeric vector with length 1 denoting the number of
+#' @param n_xx_gamma Optional numeric vector with length 1 denoting the number of
 #'   (level 2) covariates used to predict the transition probability matrix
-#'   gamma. When omitted, the model assumes no covariates are used.
+#'   gamma. When omitted, the model assumes no covariates are used to predict
+#'   gamma.
 #' @param gamma_mu0 A list containing m matrices; one matrix for each row of the
 #'   transition probability matrix gamma. Each matrix contains the hypothesized
-#'   mean values of the intercepts. Hence, each matrix consists of one row (when
-#'   not including covariates in the model) and \code{m} - 1 columns. If
-#'   covariates are used, the number of rows in each matrix in the list is
-#'   equal to 1 + n_xx (i.e., the first row corresponds to the hyper-prior mean
-#'   values of the intercepts, the subsequent rows correspond to the hyper-prior
-#'   mean values of the regression coefficients connected to each of the
-#'   covariates).
+#'   hyper-prior mean values of the intercepts of the Multinomial logit
+#'   model on the transition probabilities gamma. Hence, each matrix
+#'   consists of one row (when not including covariates in the model) and
+#'   \code{m} - 1 columns. If covariates are used, the number of rows in each
+#'   matrix in the list is equal to 1 + n_xx_gamma (i.e., the first row corresponds to
+#'   the hyper-prior mean values of the intercepts, the subsequent rows
+#'   correspond to the hyper-prior mean values of the regression coefficients
+#'   connected to each of the covariates).
 #' @param gamma_K0 Optional numeric vector with length 1 (when no covariates are
 #'   used) denoting the number of hypothetical prior subjects on which the set
-#'   of mean intercepts specified in \code{gamma_mu0} are based. When covariates
-#'   are used: Numeric vector with length 1 + n_xx denoting the number of
-#'   hypothetical prior subjects on which the set of intercepts (first value)
-#'   and set of regression coefficients (subsequent values) are based. When
-#'   omitted, the default value of 1 is used for all elements of the numeric
-#'   vector.
-#' @param gamma_nu Numeric vector with length 1 denoting the degrees of
-#'  freedom of the Inverse Wishart distribution
-#' @param gamma_V Matrix of \code{m} - 1 by \code{m} - 1 containing the
-#'  hypothesized variance-covariance matrix between the set of intercepts.
+#'   of hyper-prior mean intercepts specified in \code{gamma_mu0} are based.
+#'   When covariates are used: Numeric vector with length 1 + n_xx_gamma denoting the
+#'   number of hypothetical prior subjects on which the set of intercepts (first
+#'   value) and set of regression coefficients (subsequent values) are based.
+#' @param gamma_nu Optional numeric vector with length 1 denoting the degrees of freedom
+#'   of the hyper-prior Inverse Wishart distribution on the covariance of
+#'   the Multinomial logit intercepts.
+#' @param gamma_V Optional matrix of \code{m} - 1 by \code{m} - 1 containing the
+#'   variance-covariance matrix of the hyper-prior Inverse Wishart distribution
+#'   on the covariance of the Multinomial logit intercepts.
+#'
+#' @return \code{prior_gamma} returns an object of class \code{mHMM_prior_gamma},
+#'   containing informative hyper-prior values for the transition probability
+#'   matrix gamma of the multilevel hidden Markov model. The object is
+#'   specifically created and formatted for use by the function \code{mHMM},
+#'   and thoroughly checked for correct input dimensions.
+#'   The object contains the following components:
+#'   \describe{
+#'   \item{\code{m}}{Numeric vector denoting the number of hidden states, used
+#'   for checking equivalent general model properties specified under
+#'   \code{prior_gamma} and \code{mHMM}.}
+#'   \item{\code{gamma_mu0}}{A list containing the hypothesized hyper-prior mean
+#'   values of the intercepts of the Multinomial logit model on the transition
+#'   probability matrix gamma.}
+#'   \item{\code{gamma_K0}}{A numeric vector denoting the number of hypothetical
+#'   prior subjects on which the set of hyper-prior mean intercepts specified in
+#'   \code{gamma_mu0} are based.}
+#'   \item{\code{gamma_nu}}{A numeric vector denoting the the degrees of freedom
+#'   of the hyper-prior Inverse Wishart distribution on the covariance of the
+#'   Multinomial logit intercepts.}
+#'   \item{\code{gamma_V}}{A matrix containing the variance-covariance of the
+#'   hyper-prior Inverse Wishart distribution on the covariance of the
+#'   Multinomial logit intercepts.}
+#'   \item{\code{n_xx_gamma}}{A numeric vector denoting the number of (level 2)
+#'   covariates used to predict the tansition probability matrix gamma. When no
+#'   covariates are used, \code{n_xx_gamma} equals \code{NULL}.}
+#'   }
+#'
+#' @seealso \code{\link{prior_emiss_cat}} for manually specifying an informative
+#'   hyper-prior on the categorical emission distribution(s),
+#'   \code{\link{prob_to_int}} for transforming a set of probabilities to a set
+#'   of Multinomial logit regression intercepts, and \code{\link{mHMM}} for
+#'   fitting a multilevel hidden Markov model.
 #'
 #' @examples
+#' ###### Example using package example data, see ?nonverbal
 #' # specifying general model properties:
 #' m <- 3
 #' # representing a prior belief that switching to state 3 does not occur often and state 3 has a relative short duration
@@ -74,7 +126,7 @@
 #'
 #' gamma_K0 <- 1
 #' gamma_nu <- 5
-#' gamma_V <- diag(5, m)
+#' gamma_V <- diag(5, m - 1)
 #'
 #' manual_prior_gamma <- prior_gamma(m = m, gamma_mu0 = gamma_mu0,
 #'                                   gamma_K0 = gamma_K0, gamma_nu = gamma_nu, gamma_V = gamma_V)
@@ -118,34 +170,52 @@
 #'
 #' @export
 #'
-#' INCLUDE EXAMPLE WHERE WE USE prob_to_int directly to obtain a
 
-prior_gamma <- function(m, n_xx = NULL, gamma_mu0, gamma_K0 = NULL, gamma_nu = NULL, gamma_V = NULL){
-  if(is.null(n_xx)){
+
+prior_gamma <- function(m, gamma_mu0, gamma_K0 = NULL, gamma_nu = NULL, gamma_V = NULL, n_xx_gamma = NULL){
+  if(is.null(n_xx_gamma)){
     n_xx_int <- 1
   } else {
-    n_xx_int <- n_xx + 1
+    n_xx_int <- n_xx_gamma + 1
   }
-  if(!is.list(gamma_mu0) | length(gamma_mu0) != m){
-
+  if(!is.list(gamma_mu0) | length(gamma_mu0) != m | sum(sapply(gamma_mu0, is.matrix)) != m){
+    stop(paste("gamma_mu0 should be a list containing", m, "matrices; one matrix for each row of the transition probability matrix gamma."))
+  }
+  if(sum((m-1) == sapply(gamma_mu0, dim)[2,]) != m){
+    stop(paste("Each matrix in the list gamma_mu0 should consist of m-1 , here", m-1,", columns"))
+  }
+  if(n_xx_int == 1 & sum(1 == sapply(gamma_mu0, dim)[1,]) != m){
+    stop("According to the input paramter n_xx_gamma no covariates are used for gamma. Hence, the number of rows in each matrix of gamma_mu0 should equal 1.")
+  }
+  if(n_xx_int > 1 & sum(n_xx_int == sapply(gamma_mu0, dim)[1,]) != m ){
+    stop(paste("According to the input paramter n_xx_gamma", n_xx_gamma, "covariates are used for gamma. Hence, the number of rows in each matrix of gamma_mu0 should equal 1 + n_xx_gamma =", 1 + n_xx_gamma))
   }
   gamma_mu0 <- gamma_mu0
   if(is.null(gamma_K0)){
     gamma_K0			<- diag(1, n_xx_int)
   } else {
+    if(!is.double(gamma_K0) | length(gamma_K0) != n_xx_int){
+      stop(paste("gamma_K0 should be a numeric vector with length", n_xx_int))
+    }
     gamma_K0 <- diag(gamma_K0, n_xx_int)
   }
   if(is.null(gamma_nu)){
     gamma_nu <- 3 + m - 1
   } else {
+    if(!is.double(gamma_nu) | length(gamma_nu) != 1){
+      stop("gamma_nu should be a numeric vector with length 1")
+    }
     gamma_nu <- gamma_nu
   }
   if(is.null(gamma_V)){
     gamma_V			  <- (3 + m - 1) * diag(m - 1)
   } else {
+    if(!is.matrix(gamma_V) | dim(gamma_V)[1] != (m-1) | dim(gamma_V)[2] != (m-1)){
+      stop(paste("gamma_V should be an", m-1, "by", m-1, "matrix"))
+      }
     gamma_V <- gamma_V
   }
-  out <- list(m = m, n_xx = n_xx, gamma_mu0 = gamma_mu0, gamma_K0 = gamma_K0, gamma_nu = gamma_nu, gamma_V = gamma_V)
+  out <- list(m = m, n_xx_gamma = n_xx_gamma, gamma_mu0 = gamma_mu0, gamma_K0 = gamma_K0, gamma_nu = gamma_nu, gamma_V = gamma_V)
   class(out) <- append(class(out), "mHMM_prior_gamma")
   return(out)
 }
