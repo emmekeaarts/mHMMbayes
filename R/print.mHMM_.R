e2b74132f59c66752120e5bd81681edc233d45a3
emmekeaarts <e.aarts@uu.nl>
2023-08-11 09:35:14 +0200

    print methods for both obtain_gamma AND obtain_emiss

---
+++ R/print.mHMM_.R
@@ -0,0 +1,12 @@
+#' @export
+#'
+ print.mHMM_gamma <- function(x, ...){
+  print(x[])
+ }
+
+ #' @export
+ #'
+ print.mHMM_emiss <- function(x, ...){
+   print(x[])
+ }
+
--- R/print.mHMM_gamma.R
+++
@@ -1,5 +0,0 @@
-#' @export
-#'
- print.mHMM_gamma <- function(x, ...){
-  print(x[])
-}
