db790049ce98eb3c51cbbf2d19949a32b26238a5
emmekeaarts <e.aarts@uu.nl>
2023-08-11 20:12:06 +0200

    added missing package dependency

--- R/mHMMbayes.R
+++ R/mHMMbayes.R
@@ -54,7 +54,7 @@
 # not sure if all functions given below for packages are actually still used, check!
 #' @importFrom mvtnorm dmvnorm rmvnorm dmvt rmvt
 #' @importFrom MCMCpack rdirichlet rwish
-#' @importFrom stats optim rnorm runif median na.omit
+#' @importFrom stats optim rnorm runif median na.omit quantile
 # to include bib references in help file
 #' @importFrom Rdpack reprompt
 # for RCpp
